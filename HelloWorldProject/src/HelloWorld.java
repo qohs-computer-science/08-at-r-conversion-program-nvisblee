88be8cefbde1262dc1444e665a4c9d821620d42f
Arun M <154700060+nvisblee@users.noreply.github.com>
2025-09-18 17:37:55 +0000

    tried to create two fdifferent methods but Need to fix these

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,13 +1,62 @@
+import java.util.Scanner;
+
+
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * TODO: Arun Mariappan
+ * TODO: 9/18/25
+ * TODO: 4
+ * TODO: converts decimal to binary and binary to deimal. 
  */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		//TODO: Implement Program Requirements Here
+		Scanner myScanner = new Scanner(System.in);
+
+		boolean runAgain = true; 
+		while(runAgain) { 
+			System.out.println("Welcome!  You have the following options:");
+			System.out.println("1. Binary to Decimal");
+			System.out.println("2. Decimal to Binary");
+			System.out.print("Which type of conversion would you like to perform? (type 1 or 2)");
+			int type = myScanner.nextInt();
+			if(type == 1) {
+				System.out.println("What is the binary number?");
+				String binary = myScanner.nextLine();
+				
+
+
+
+		}
+		
+		else if (type == 2) {
+			System.out.println("What is the Decimal number?");
+			String decimal = myScanner.nextLine();
+			int ans = binaryToDecimal(decimal);
+
+
+
+			
+
+		}
+	}
+
+
+	public static String decimalToBinary (int bin) {
+		String binaryAsNum = Integer.toBinaryString(bin);		
+		return binaryAsNum;
+
+	}
+	public static int binaryToDecimal (String dec) {
+		int decimalAsNum = Integer.parseInt(dec); 
+		return decimalAsNum; 
+		
+
+
+	}
+		
+		
+	myScanner.close(); 
 	}
 
 }
