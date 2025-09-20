a7def0ded978e6d1ebaf50555aeb291a7533d065
Arun M <154700060+nvisblee@users.noreply.github.com>
2025-09-20 03:51:06 +0000

    Finished and finalized the code!!!

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; // import the Scanner class to let user input 
 
 
 /*
@@ -20,43 +20,56 @@ public class HelloWorld {
 			System.out.println("2. Decimal to Binary");
 			System.out.print("Which type of conversion would you like to perform? (type 1 or 2)");
 			int type = myScanner.nextInt();
+			myScanner.nextLine(); 
+			
+			
 			if(type == 1) {
 				System.out.println("What is the binary number?");
 				String binary = myScanner.nextLine();
-				
-
-
-
-		}
+				System.out.println("The decimal value is " + binaryToDecimal(binary));
+			}
 		
-		else if (type == 2) {
-			System.out.println("What is the Decimal number?");
-			String decimal = myScanner.nextLine();
-			int ans = binaryToDecimal(decimal);
-
-
-
+			else if (type == 2) {
+				System.out.println("What is the Decimal number?");
+				String decimal = myScanner.nextLine();
+				System.out.println("The binary value is " + decimalToBinary(Integer.parseInt(decimal)));
+			}
 			
-
+			System.out.print("Do you want to continue - yes or no?");
+			String again = myScanner.nextLine();
+			if(again.equals("no")) {
+				runAgain = false;
+			}
 		}
+	
+		myScanner.close();
 	}
-
 
 	public static String decimalToBinary (int bin) {
-		String binaryAsNum = Integer.toBinaryString(bin);		
-		return binaryAsNum;
+		if (bin == 0) {
+			return "0"; 
+		}
+
+		String result = "";
+		while (bin > 0) {
+			int remainder = bin % 2; 
+			result = remainder + result; 
+			bin = bin / 2; 
+		}
+		return result;
 
 	}
+
 	public static int binaryToDecimal (String dec) {
-		int decimalAsNum = Integer.parseInt(dec); 
-		return decimalAsNum; 
-		
-
-
-	}
-		
-		
-	myScanner.close(); 
-	}
-
-}
+		int decimalValue = 0;
+		int power = 0;
+		for (int i = dec.length() - 1; i >= 0; i--) {
+			char c = dec.charAt(i);
+			if (c == '1') {
+				decimalValue += Math.pow(2, power);
+			}
+			power++;
+		}
+		return decimalValue;
+	} // end main
+} // end class
